dcb742d22e865be8d4239d7d2add362e2065e42e
Fernando <Fernando@DESKTOP-3EE8PP0.home>
2023-05-24 19:07:07 +0200

    Terminado

--- Tema10-Ficheros/src/ejercicio6/Ejercicio6.java
+++ Tema10-Ficheros/src/ejercicio6/Ejercicio6.java
@@ -11,24 +11,40 @@ import java.util.Scanner;
 
 public class Ejercicio6 {
 	public static void main(String[] args) {
+		int numero;
 		ArrayList<Integer> coleccion = new ArrayList<Integer>();
+		BufferedWriter bw = null;
+		Scanner read = null;
 		try {
-			Scanner read = new Scanner(new FileReader("src\\ejercicio6\\EnterosDesor.txt"));
+			read = new Scanner(new FileReader("src\\ejercicio6\\EnterosDesor.txt"));
 			while (read.hasNext()) {
 				coleccion.add(read.nextInt());
-				Collections.sort(coleccion);
-				try {
-					BufferedWriter bw = new BufferedWriter(new FileWriter("src\\ejercicio6\\EnterosOrden.txt"));
-					bw.write(coleccion);
-					bw.flush();
-				} catch (IOException e) {
-					System.out.println("No existe el fichero");
-					e.getMessage();
-				}
+
 			}
+
+			Collections.sort(coleccion);
+
+			bw = new BufferedWriter(new FileWriter("src\\ejercicio6\\EnterosOrden.txt"));
+
+			for (Integer valores : coleccion) {
+				bw.write(String.valueOf(valores));
+				bw.newLine();
+			}
+
 		} catch (FileNotFoundException e) {
 			System.out.println("No se encuentra el fichero");
 			e.getMessage();
+		} catch (IOException e) {
+			System.out.println("El fichero no se puede abrir");
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+				bw.flush();
+				bw.close();
+				read.close();
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
 		}
 	}
 }
